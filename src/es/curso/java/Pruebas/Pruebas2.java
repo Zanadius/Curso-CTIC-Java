fcfc287de7470439e1bfd62678347da13be4bb35
Carlos <Carlos@DESKTOP-0BSDEFL>
2023-09-12 16:47:48 +0200

    Prueba

--- src/es/curso/java/Pruebas/Pruebas2.java
+++ src/es/curso/java/Pruebas/Pruebas2.java
@@ -6,20 +6,12 @@ public class Pruebas2 {
 	public static void main(String[] arg) {
 
 		// Lo único que se puede cambiar de la función es el "arg".
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in); 
 
 		// Aulas del colegio
 		System.out.println("Introduzca el numero de aulas: ");
 		int numeroAulas = scan.nextInt();
 		String[][] aulas = new String[numeroAulas][];
 
-		// Estudiantes * aula
-		for (int i = 0; i < aulas.length; i++, numeroAulas++) {
-			System.out.println("Introduzca el numero de estudiantes del aula: " + (i + 1));
-			int numeroEstudiantes = scan.nextInt();
-			scan.nextLine();
-			aulas[i] = new String[numeroEstudiantes];
-
 		}
 	}
-}
